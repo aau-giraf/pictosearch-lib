a3fcbf86732b86eeaab6efb06bc49e7e393f27eb
Tobias <tobiasfrandsen4@gmail.com>
2014-03-25 14:57:09 +0100

    Category search function

--- src/dk/aau/cs/giraf/pictosearch/SearchClass.java
+++ src/dk/aau/cs/giraf/pictosearch/SearchClass.java
@@ -21,9 +21,9 @@ public class SearchClass
         if (tag == "Tag") return DoSearch_Tags(input, AllPictograms);
         else return DoSearch_All(input, AllPictograms);
     }
-    public ArrayList<Object> DoCategorySearch(String tag, String[] input, Long ChildID, ArrayList<PARROTCategory> AllCategories)
+    public ArrayList<Object> DoCategorySearch(String[] input, ArrayList<PARROTCategory> AllCategories)
     {
-        return DoSearch_Category(input, ChildID, AllCategories);
+        return DoSearch_Category(input, AllCategories);
     }
 
     // PICTOGRAM IMPLEMENTATIONS
@@ -77,12 +77,41 @@ public class SearchClass
 
     // CATEGORY IMPLEMENTATIONS
 
-    private ArrayList<Object> DoSearch_Category(String[] input, Long ChildID, ArrayList<PARROTCategory> CatSearchList)
+    private ArrayList<Object> DoSearch_Category(String[] input, ArrayList<PARROTCategory> CatSearchList)
     {
-        //ArrayList<PARROTCategory> CatSearchList = new ArrayList<PARROTCategory>();
-        //CatSearchList = CatHelp.getChildsCategories(ChildID);
-        ArrayList<Object> CatSearchList2 = new ArrayList<Object>();
+        ArrayList<Object> lst = new ArrayList<Object>();
 
-        return CatSearchList2;
+
+        for (PARROTCategory pc : CatSearchList){
+            boolean added = false;
+            for (int i = 0; i < input.length; i++){
+                if (pc.getCategoryName().contains(input[i])){
+                    lst.add(pc);
+                    added = true;
+                    break;
+                }
+                else if (pc.getSubCategories() != null){
+                    for (PARROTCategory spc : pc.getSubCategories()){
+                        if (spc.getCategoryName().contains(input[i])){
+                            lst.add(pc);
+                            added = true;
+                            break;
+                        }
+                    }
+                }
+                else if (pc.getPictograms() != null){
+                    for (Pictogram p : pc.getPictograms()){
+                        if (p.getName().contains(input[i])){
+                            lst.add(pc);
+                            added = true;
+                            break;
+                        }
+                    }
+                }
+                if (added) break;
+            }
+        }
+
+        return lst;
     }
 }
