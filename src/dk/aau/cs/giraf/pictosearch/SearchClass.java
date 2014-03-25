48d0505f12385a6406440446631ecc3e69760553
Tobias <tobiasfrandsen4@gmail.com>
2014-03-25 13:36:32 +0100

    no message

--- src/dk/aau/cs/giraf/pictosearch/SearchClass.java
+++ src/dk/aau/cs/giraf/pictosearch/SearchClass.java
@@ -4,4 +4,22 @@ package dk.aau.cs.giraf.pictosearch;
  * Created by Tobias on 25-03-14.
  */
 public class SearchClass {
+
+    private void AllSearch(){
+        PictogramSearch();
+        CategorySearch();
+        SubCategorySearch();
+    }
+
+    private void PictogramSearch(){
+
+    }
+
+    private void CategorySearch(){
+
+    }
+
+    private void SubCategorySearch(){
+
+    }
 }
