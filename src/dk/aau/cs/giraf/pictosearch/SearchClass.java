a5d06898024aaeef2e02f0dbb4137b7aaa36afc4
Jonas <llamabutlermecha@gmail.com>
2014-03-25 14:06:22 +0100

    SearchClass now returns objects instead

--- src/dk/aau/cs/giraf/pictosearch/SearchClass.java
+++ src/dk/aau/cs/giraf/pictosearch/SearchClass.java
@@ -12,28 +12,33 @@ import java.util.List;
  */
 public class SearchClass
 {
-    public ArrayList<Pictogram> DoSearch(String tag, String[] input)
+    public ArrayList<Object> DoSearch(String tag, String[] input)
     {
         // Vælg tag, resten skal tilføjes på et tidspunkt
         if (tag == "Tag") return DoSearch_Tags(input);
         else return DoSearch_All(input);
     }
-    public ArrayList<PARROTCategory> DoCategorySearch(String tag, String[] input)
+    public ArrayList<Object> DoCategorySearch(String tag, String[] input)
     {
         return DoSearch_Category(input);
     }
 
-    private ArrayList<Pictogram> DoSearch_All(String[] input)
+    private ArrayList<Object> DoSearch_All(String[] input)
+    {
+        // Combines name and category, but make sure to avoid duplicates!!
+    }
+
+    private ArrayList<Object> DoSearch_Name(String[] input)
     {
 
     }
 
-    private ArrayList<Pictogram> DoSearch_Tags(String[] input)
+    private ArrayList<Object> DoSearch_Tags(String[] input)
     {
 
     }
 
-    private ArrayList<PARROTCategory> DoSearch_Category(String[] input)
+    private ArrayList<Object> DoSearch_Category(String[] input)
     {
 
     }
