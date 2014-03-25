9a4f40502ddb0060aa8d033ecd0e1b521113c162
Jonas <llamabutlermecha@gmail.com>
2014-03-25 13:42:20 +0100

    SearchClass divided into multiple methods

--- src/dk/aau/cs/giraf/pictosearch/SearchClass.java
+++ src/dk/aau/cs/giraf/pictosearch/SearchClass.java
@@ -1,25 +1,39 @@
 package dk.aau.cs.giraf.pictosearch;
 
+import dk.aau.cs.giraf.pictogram.Pictogram;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Created by Tobias on 25-03-14.
  */
-public class SearchClass {
-
-    private void AllSearch(){
-        PictogramSearch();
-        CategorySearch();
-        SubCategorySearch();
+public class SearchClass
+{
+    public ArrayList<Pictogram> DoSearch(String tag, string input)
+    {
+        // Vælg tag, resten skal tilføjes på et tidspunkt
+        if (tag == "category") return DoSearch_Category(input);
+        else return DoSearch_All(input);
     }
-
-    private void PictogramSearch(){
-
-    }
-
-    private void CategorySearch(){
+    private ArrayList<Pictogram> DoSearch_All(string input)
+    {
 
     }
 
-    private void SubCategorySearch(){
+    private ArrayList<Pictogram> DoSearch_Pictogram(string input)
+    {
+
+    }
+
+    private ArrayList<Pictogram> DoSearch_Category(string input)
+    {
+
+    }
+
+    private ArrayList<Pictogram> DoSearch_SubCategory(string input)
+    {
 
     }
 }
