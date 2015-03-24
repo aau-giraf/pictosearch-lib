022353bcaaa79a7c588cd3b5b395e274a1f7dc2a
Mafaias <Mtoust12@student.aau.dk>
2015-03-24 14:47:12 +0100

    Step 3 MessageDialogFragment Code Improvement.

--- src/main/java/dk/aau/cs/giraf/pictosearch/MessageDialogFragment.java
+++ src/main/java/dk/aau/cs/giraf/pictosearch/MessageDialogFragment.java
@@ -10,16 +10,24 @@ import android.os.Bundle;
 
 /**
  * Used to display a message to the user. Message is included in constructor
- * @author Christian Klim Hansen, SW605f13 Parrot-group
  */
 @SuppressLint("ValidFragment")
 public class MessageDialogFragment extends DialogFragment {
-	public String message;
-		
-	public MessageDialogFragment(String msg) {
-		this.message = msg;
+	private final String message;
+
+    /**
+     * Set this.message to message
+     * @param message input string for dialog message
+     */
+	public MessageDialogFragment(String message) {
+		this.message = message;
 	}
-	
+
+    /**
+     *
+     * @param savedInstanceState A mapping from String values to various Parcelable types.
+     * @return the AlertDialog object
+     */
 	@Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
@@ -31,7 +39,6 @@ public class MessageDialogFragment extends DialogFragment {
 				//Do nothing
 			}
 		});
-        // Create the AlertDialog object and return it
         return builder.create();
     }
 }
