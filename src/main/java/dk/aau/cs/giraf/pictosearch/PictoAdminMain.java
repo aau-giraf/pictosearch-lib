5e3e9f56724e21dad5e0d2806119e1ae0c2ad0e0
Patrick Grønhøj <pgronh12@student.aau.dk>
2015-05-05 15:21:13 +0200

    Removed 2 lines of code.

--- src/main/java/dk/aau/cs/giraf/pictosearch/PictoAdminMain.java
+++ src/main/java/dk/aau/cs/giraf/pictosearch/PictoAdminMain.java
@@ -53,8 +53,6 @@ public class PictoAdminMain extends GirafActivity implements AsyncResponse{
 
     public GridView checkoutGrid;
     private GridView pictoGrid;
-    public RelativeLayout r1;
-    public ImageView catIndicatorView;
 
     /*
      *  Request from another group. It should be possible to only send one pictogram,
